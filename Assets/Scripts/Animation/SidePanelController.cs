c07550a05d9ab08dfb0a745b5639b6972c84fc1d
Vultys <vlad.serdiuk750@gmail.com>
2025-04-11 16:49:07 +0300

    Оптимізація анімацій під SafeArea

--- Assets/Scripts/Animation/SidePanelController.cs
+++ Assets/Scripts/Animation/SidePanelController.cs
@@ -9,6 +9,10 @@ public class SidePanelController : MonoBehaviour
     [SerializeField] private RectTransform _panel;
     [SerializeField] private List<Transform> _slots;
     [SerializeField] private RectTransform _hidePanelButton;
+    [SerializeField] private Transform _defaultHidePanelButtonPosition;
+    [SerializeField] private Transform _defaultPanelPosition;
+    [SerializeField] private Transform _hiddenPanelPosition;
+    [SerializeField] private Transform _foldedHidePanelButtonPosition;
     public bool IsVisible => _isVisible; 
     private bool _isVisible = true;
 
@@ -27,15 +31,15 @@ public class SidePanelController : MonoBehaviour
     public IEnumerator ShowPanel()
     {
         _isVisible = true;
-        yield return _hidePanelButton.DOLocalMoveX(696.5f, 0.5f).SetEase(Ease.InCubic).WaitForCompletion();
-        yield return _panel.DOLocalMoveX(847.5f, 0.5f).SetEase(Ease.OutCubic).WaitForCompletion();
+        yield return _hidePanelButton.DOMoveX(_defaultHidePanelButtonPosition.position.x, 0.5f).SetEase(Ease.InCubic).WaitForCompletion();
+        yield return _panel.DOMoveX(_defaultPanelPosition.position.x, 0.5f).SetEase(Ease.OutCubic).WaitForCompletion();
     }
 
     public IEnumerator HidePanel()
     {
         _isVisible = false;
-        yield return _panel.DOLocalMoveX(1200f, 0.5f).SetEase(Ease.InCubic).WaitForCompletion();
-        yield return _hidePanelButton.DOLocalMoveX(900f, 0.5f).SetEase(Ease.OutCubic).WaitForCompletion();
+        yield return _panel.DOMoveX(_hiddenPanelPosition.position.x, 0.5f).SetEase(Ease.InCubic).WaitForCompletion();
+        yield return _hidePanelButton.DOMoveX(_foldedHidePanelButtonPosition.position.x, 0.5f).SetEase(Ease.OutCubic).WaitForCompletion();
     }
     
     public Transform GetNextAvailableSlot()
